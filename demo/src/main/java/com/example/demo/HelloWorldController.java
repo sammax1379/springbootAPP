125f7ad400f7baf466459c338bb77f35be1ab6d3
sammax1379 <sammas45268@gmail.com>
2025-05-06 03:59:06 +0000

    init1

--- demo/src/main/java/com/example/demo/HelloWorldController.java
+++ demo/src/main/java/com/example/demo/HelloWorldController.java
@@ -2,14 +2,14 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
 public class HelloWorldController {
     @GetMapping("hello")
     public String getMethodName() {
-        return "hello world!";
+        return "Hello World!";
     }
-    
     
 }
